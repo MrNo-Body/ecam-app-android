49f4e5afc57be2dfab71a9949d26641f93b065f2
Christophe DEGRELLE <11033@ecam.be>
2017-03-28 09:34:28 +0200

    Initialise android project

---
+++ app/src/main/java/be/ecam/chowdetails/chowdetails/FoodMainActivity.java
@@ -0,0 +1,13 @@
+package be.ecam.chowdetails.chowdetails;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_main);
+    }
+}
