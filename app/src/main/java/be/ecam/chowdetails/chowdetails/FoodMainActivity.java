71e22680aa62e4a399347dd1cdb73d28528a1842
Neilack <tireur@live.be>
2017-04-24 17:49:24 +0200

    Option Button Added

--- app/src/main/java/be/ecam/chowdetails/chowdetails/FoodMainActivity.java
+++ app/src/main/java/be/ecam/chowdetails/chowdetails/FoodMainActivity.java
@@ -2,6 +2,9 @@ package be.ecam.chowdetails.chowdetails;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.app.Activity;
 import android.view.View;
@@ -19,12 +22,26 @@ public class FoodMainActivity extends AppCompatActivity implements OnClickListen
         Button button01 = (Button) findViewById(R.id.button01);
 
         button01.setOnClickListener(this);
-
-
-        //
-
-
     }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.Option:
+                setContentView(R.layout.preference);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 
     public void onClick(View view) {
         if (view.getId() == R.id.button01) {
