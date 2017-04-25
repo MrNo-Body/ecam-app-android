4ae9f73304e960692aa925373f1a6ea2e90890c2
Aladdin77 <13058@ecam.be>
2017-04-25 10:18:19 +0200

    Button of the main page leads to different layout => to implement these layout

--- app/src/main/java/be/ecam/chowdetails/chowdetails/FoodMainActivity.java
+++ app/src/main/java/be/ecam/chowdetails/chowdetails/FoodMainActivity.java
@@ -2,17 +2,32 @@ package be.ecam.chowdetails.chowdetails;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.text.InputType;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.app.Activity;
+import android.view.View.OnClickListener;
 
-public class FoodMainActivity extends AppCompatActivity {
+
+
+public class FoodMainActivity extends AppCompatActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_main);
+
+        Button butfavoris= (Button) findViewById(R.id.butfavoris);
+        butfavoris.setOnClickListener(this);
+        Button butfind= (Button) findViewById(R.id.butfind);
+        butfind.setOnClickListener(this);
 
         /* test des differents widget
         TextView text = new TextView(this);
@@ -30,5 +45,19 @@ public class FoodMainActivity extends AppCompatActivity {
         button.setText("button");
         setContentView(button);*/
 
+
+    }
+    public void onClick(View view)
+    {
+        if (view.getId() == R.id.butfind)
+        {
+            Toast.makeText(this, "page de ismael", Toast.LENGTH_SHORT).show();
+            setContentView(R.layout.activity_food_search);
+        }
+        if (view.getId() == R.id.butfavoris)
+        {
+            Toast.makeText(this, "page de favoris", Toast.LENGTH_SHORT).show();
+            setContentView(R.layout.activity_food_favorite);
+        }
     }
 }
